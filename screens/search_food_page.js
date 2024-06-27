d5523d562a02b42cdaed901b69336bfa087afbf6
romandicesarefernandes <romandicesarefer@gmail.com>
2024-06-27 14:21:45 -0400

    Staarted search page

--- screens/search_food_page.js
+++ screens/search_food_page.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { handleFood_search } from "../scripts/handle_register";
+let images = [];
+async function getFoodData(ingr, brand) {
+  const foodData = await handleFood_search(ingr, brand);
+
+  for (let i = 0; i < foodData.length; i++) {
+    console.log(foodData[i].food.image);
+    console.log("\n" + foodData[i].food.knownAs);
+    images[i] = foodData[i].food.image;
+  }
+  console.log("\n" + images[1]);
+  return foodData;
+}
 
 const SearchFoodPage = () => {
+  const data = getFoodData("rice", "royal");
+
+  console.log("\n" + data);
+
   return (
     <View style={styles.container}>
-      <Text>Search Food Page</Text>
+      <Image
+        source={{
+          uri: images[1],
+        }}
+        style={{ width: 100, height: 100 }}
+      />
+      <Text>Data</Text>
     </View>
   );
 };
@@ -13,6 +36,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+
     alignItems: "center",
   },
 });
--- scripts/handle_register.js
+++ scripts/handle_register.js
@@ -1,5 +1,5 @@
 export function handleRegister(name, password, phoneNumber, email) {
-  fetch("http://192.168.1.37:5000/api/register", {
+  fetch("http://192.168.1.24:5000/api/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -22,7 +22,7 @@ export function handleRegister(name, password, phoneNumber, email) {
 }
 
 export function handleLogin(email, password) {
-  return fetch("http://192.168.1.37:5000/api/login", {
+  return fetch("http://192.168.1.24:5000/api/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -45,8 +45,37 @@ export function handleLogin(email, password) {
 }
 export async function handleFood_search(ingr, brand) {
   try {
-    const url = new URL("http://192.168.1.37:5000/api/food_search");
+    const url = new URL("http://192.168.1.24:5000/api/food_search");
     const params = { ingr, brand };
+    console.log(params);
+    Object.keys(params).forEach((key) =>
+      url.searchParams.append(key, params[key])
+    );
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json(); // Use text() to get the response as a string
+
+    return data; // Return the string data
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+}
+
+export async function handleFood_request_nutrients(ingr) {
+  try {
+    const url = new URL("http://192.168.1.24:5000/api/food_search");
+    const params = { ingr };
     Object.keys(params).forEach((key) =>
       url.searchParams.append(key, params[key])
     );
